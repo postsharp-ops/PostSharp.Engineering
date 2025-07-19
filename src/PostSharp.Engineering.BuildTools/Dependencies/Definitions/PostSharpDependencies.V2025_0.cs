d1a482834d9959247ab503a0975a3af2406c966e
Gael Fraiteur <gael@postsharp.net>
2025-07-19 13:05:58 +0200

    Added product definitions for PostSharp on GitHub, with the new directory structure.

--- src/PostSharp.Engineering.BuildTools/Dependencies/Definitions/PostSharpDependencies.V2025_0.cs
+++ src/PostSharp.Engineering.BuildTools/Dependencies/Definitions/PostSharpDependencies.V2025_0.cs
@@ -9,7 +9,7 @@ namespace PostSharp.Engineering.BuildTools.Dependencies.Definitions;
 
 public static partial class PostSharpDependencies
 {
-    // ReSharper disable once InconsistentNaming
+    // ReSharper disable InconsistentNaming
 
     [PublicAPI]
     public static class V2025_0
@@ -39,6 +39,42 @@ public static partial class PostSharpDependencies
                     false )
             {
                 this.EngineeringDirectory = @"PrivateBuild\Distribution\eng";
+            }
+        }
+
+        public static ProductFamily Family { get; } = new( _projectName, "2025.0", DevelopmentDependencies.Family );
+
+        public static DependencyDefinition PostSharp { get; } = new PostSharpDependencyDefinition();
+    }
+    
+    [PublicAPI]
+    public static class V2025_0_GitHub
+    {
+        private class PostSharpDependencyDefinition : DependencyDefinition
+        {
+            private static readonly TeamCityProjectId _teamCityProjectId = new(
+                $"{_projectName}_{_projectName}{Family.VersionWithoutDots}",
+                _projectName );
+
+            private static readonly string _distributionBuildId = $"{_teamCityProjectId}_BuildDistribution";
+
+            public PostSharpDependencyDefinition()
+                : base(
+                    Family,
+                    "PostSharpPackage",
+                    $"release/{Family.Version}",
+                    null,
+                    new GitHubRepository( _projectName, _projectName ),
+                    new CiProjectConfiguration(
+                        _teamCityProjectId,
+                        new ConfigurationSpecific<string>( "not-used", _distributionBuildId, "not-used" ),
+                        null,
+                        null,
+                        TeamCityHelper.TeamCityCloudTokenEnvironmentVariableName,
+                        TeamCityHelper.TeamCityCloudUrl ),
+                    false )
+            {
+                this.EngineeringDirectory = @"Build\Distribution\eng";
             }
         }
 
