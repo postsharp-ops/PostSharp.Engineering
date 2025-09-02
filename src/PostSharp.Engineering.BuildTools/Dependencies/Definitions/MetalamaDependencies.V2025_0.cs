c149867cd649011751e3ce4e427fd2b3906cb481
Daniel Balas <daniel@postsharp.net>
2025-09-02 15:25:11 +0200

    Bring back 2025.0 because the support will continue for a few months.

---
+++ src/PostSharp.Engineering.BuildTools/Dependencies/Definitions/MetalamaDependencies.V2025_0.cs
@@ -0,0 +1,171 @@
+// Copyright (c) SharpCrafters s.r.o. See the LICENSE.md file in the root directory of this repository root for details.
+
+using JetBrains.Annotations;
+using PostSharp.Engineering.BuildTools.ContinuousIntegration;
+using PostSharp.Engineering.BuildTools.ContinuousIntegration.Model;
+using PostSharp.Engineering.BuildTools.Dependencies.Model;
+using PostSharp.Engineering.BuildTools.Docker;
+using System;
+using System.IO;
+using System.Runtime.InteropServices.Marshalling;
+
+namespace PostSharp.Engineering.BuildTools.Dependencies.Definitions;
+
+public static partial class MetalamaDependencies
+{
+    // ReSharper disable once InconsistentNaming
+
+    [PublicAPI]
+    public static class V2025_0
+    {
+        private class MetalamaDependencyDefinition : DependencyDefinition
+        {
+            public MetalamaDependencyDefinition(
+                string dependencyName,
+                VcsProvider vcsProvider,
+                MetalamaGitHubOrganization? organization,
+                bool isVersioned = true,
+                string? parentCiProjectId = null,
+                string? customCiProjectName = null,
+                string? customBranch = null,
+                string? customReleaseBranch = null,
+                string? customRepositoryName = null,
+                bool pullRequestRequiresStatusCheck = true,
+                string? vcsRootProjectId = null )
+                : base(
+                    Family,
+                    dependencyName,
+                    customBranch ?? $"develop/{Family.Version}",
+                    customReleaseBranch ?? $"release/{Family.Version}",
+                    CreateMetalamaVcsRepository(
+                        customRepositoryName ?? dependencyName,
+                        vcsProvider,
+                        organization,
+                        customBranch == null && customReleaseBranch == null
+                            ? null
+                            : $"DefaultBranch_{dependencyName.Replace( ".", "", StringComparison.Ordinal )}" ),
+                    TeamCityHelper.CreateConfiguration(
+                        parentCiProjectId == null
+                            ? TeamCityHelper.GetProjectId( dependencyName, _projectName, Family.Version )
+                            : TeamCityHelper.GetProjectIdWithParentProjectId( dependencyName, parentCiProjectId ),
+                        isVersioned,
+                        pullRequestRequiresStatusCheck: pullRequestRequiresStatusCheck,
+                        vcsRootProjectId: vcsRootProjectId ),
+                    isVersioned ) { }
+        }
+
+        public static ProductFamily Family { get; } = new( _projectName, "2025.0", DevelopmentDependencies.Family, PostSharpDependencies.V2025_0.Family )
+        {
+            DockerBaseImage = DockerImages.WindowsServerCore,
+
+            // Manual merging is required from-non merged repositories to 2025.1.
+
+            // UpstreamProductFamily = V2024_1.Family,
+            // DownstreamProductFamily = V2025_1.Family
+        };
+
+        public static DependencyDefinition Consolidated { get; } =
+            new MetalamaDependencyDefinition(
+                ProductFamily.ConsolidatedProjectName,
+                VcsProvider.AzureDevOps,
+                null,
+                false,
+                customRepositoryName: "Metalama.Consolidated" );
+
+        // The release build is intentionally used for the debug configuration because we want dependencies to consume the release
+        // build, for performance reasons. The debug build will be used only locally, and for this we don't need a configuration here.
+        public static DependencyDefinition MetalamaCompiler { get; } =
+            new MetalamaDependencyDefinition(
+                "Metalama.Compiler",
+                VcsProvider.GitHub,
+                MetalamaGitHubOrganization.Metalama )
+            {
+                EngineeringDirectory = "eng-Metalama",
+                PrivateArtifactsDirectory = Path.Combine( "artifacts", "packages", "$(MSSBuildConfiguration)", "Shipping" )
+            };
+
+        public static DependencyDefinition Metalama { get; } =
+            new MetalamaDependencyDefinition(
+                "Metalama",
+                VcsProvider.GitHub,
+                MetalamaGitHubOrganization.Metalama )
+            {
+                // SuppressUpstream = true
+            };
+
+        public static DependencyDefinition MetalamaPremium { get; } =
+            new MetalamaDependencyDefinition(
+                "Metalama.Premium",
+                VcsProvider.GitHub,
+                MetalamaGitHubOrganization.Metalama )
+            {
+                // SuppressUpstream = true
+            };
+
+        public static DependencyDefinition MetalamaVsx { get; } =
+            new MetalamaDependencyDefinition(
+                "Metalama.Vsx",
+                VcsProvider.AzureDevOps,
+                null );
+
+        public static DependencyDefinition MetalamaSamples { get; } =
+            new MetalamaDependencyDefinition(
+                "Metalama.Samples",
+                VcsProvider.GitHub,
+                MetalamaGitHubOrganization.Metalama ) { CodeStyle = "Metalama.Samples" };
+
+        public static DependencyDefinition TimelessDotNetEngineer { get; } =
+            new MetalamaDependencyDefinition(
+                "TimelessDotNetEngineer",
+                VcsProvider.GitHub,
+                MetalamaGitHubOrganization.PostSharp ) { CodeStyle = "Metalama.Samples" };
+
+        public static DependencyDefinition MetalamaCommunity { get; } =
+            new MetalamaDependencyDefinition(
+                "Metalama.Community",
+                VcsProvider.GitHub,
+                MetalamaGitHubOrganization.PostSharp );
+
+        public static DependencyDefinition MetalamaDocumentation { get; } =
+            new MetalamaDependencyDefinition(
+                "Metalama.Documentation",
+                VcsProvider.GitHub,
+                MetalamaGitHubOrganization.Metalama,
+                false );
+
+        public static DependencyDefinition NopCommerce { get; } =
+            new MetalamaDependencyDefinition(
+                "Metalama.Tests.NopCommerce",
+                VcsProvider.GitHub,
+                MetalamaGitHubOrganization.PostSharp,
+                false,
+                parentCiProjectId: $"Metalama_Metalama{Family.VersionWithoutDots}_MetalamaTests",
+                vcsRootProjectId: $"Metalama_Metalama{Family.VersionWithoutDots}",
+                customBranch: $"dev/{Family.Version}" );
+
+        public static DependencyDefinition CargoSupport { get; } =
+            new MetalamaDependencyDefinition(
+                "Metalama.Tests.CargoSupport",
+                VcsProvider.AzureDevOps,
+                null,
+                false,
+                parentCiProjectId: $"Metalama_Metalama{Family.VersionWithoutDots}_MetalamaTests",
+                vcsRootProjectId: $"Metalama_Metalama{Family.VersionWithoutDots}" );
+
+        public static DependencyDefinition DotNetSdkTests { get; } =
+            new MetalamaDependencyDefinition(
+                "Metalama.Tests.DotNetSdk",
+                VcsProvider.GitHub,
+                MetalamaGitHubOrganization.PostSharp,
+                false,
+                parentCiProjectId: $"Metalama_Metalama{Family.VersionWithoutDots}_MetalamaTests",
+                vcsRootProjectId: $"Metalama_Metalama{Family.VersionWithoutDots}" );
+
+        public static DependencyDefinition MetalamaPerformance { get; } =
+            new MetalamaDependencyDefinition(
+                "Metalama.Performance",
+                VcsProvider.GitHub,
+                MetalamaGitHubOrganization.PostSharp,
+                false );
+    }
+}
---
+++ src/PostSharp.Engineering.BuildTools/Dependencies/Definitions/PostSharpDependencies.V2025_0.cs
@@ -0,0 +1,48 @@
+// Copyright (c) SharpCrafters s.r.o. See the LICENSE.md file in the root directory of this repository root for details.
+
+using JetBrains.Annotations;
+using PostSharp.Engineering.BuildTools.ContinuousIntegration;
+using PostSharp.Engineering.BuildTools.ContinuousIntegration.Model;
+using PostSharp.Engineering.BuildTools.Dependencies.Model;
+
+namespace PostSharp.Engineering.BuildTools.Dependencies.Definitions;
+
+public static partial class PostSharpDependencies
+{
+    // ReSharper disable once InconsistentNaming
+
+    [PublicAPI]
+    public static class V2025_0
+    {
+        private class PostSharpDependencyDefinition : DependencyDefinition
+        {
+            private static readonly TeamCityProjectId _teamCityProjectId = new(
+                $"PostSharpGitHub_{_projectName}{Family.VersionWithoutDots}", "PostSharpGitHub" );
+
+            private static readonly string _distributionBuildId = $"{_teamCityProjectId}_BuildDistribution";
+
+            public PostSharpDependencyDefinition()
+                : base(
+                    Family,
+                    "PostSharpPackage",
+                    $"refs/heads/release/{Family.Version}",
+                    null,
+                    new GitHubRepository( _projectName, _projectName ),
+                    new CiProjectConfiguration(
+                        _teamCityProjectId,
+                        new ConfigurationSpecific<string>( "not-used", _distributionBuildId, "not-used" ),
+                        null,
+                        null,
+                        TeamCityHelper.TeamCityCloudTokenEnvironmentVariableName,
+                        TeamCityHelper.TeamCityCloudUrl ),
+                    false )
+            {
+                this.EngineeringDirectory = @"Build\Distribution\eng";
+            }
+        }
+
+        public static ProductFamily Family { get; } = new( _projectName, "2025.0", DevelopmentDependencies.Family );
+
+        public static DependencyDefinition PostSharp { get; } = new PostSharpDependencyDefinition();
+    }
+}
