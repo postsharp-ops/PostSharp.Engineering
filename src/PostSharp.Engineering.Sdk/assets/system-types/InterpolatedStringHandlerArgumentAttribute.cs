c10f5a1d394c801d6526a1722367c7a8d8e3626e
Gael Fraiteur <gael@postsharp.net>
2025-09-15 13:32:53 +0200

    Adding more filling types.

---
+++ src/PostSharp.Engineering.Sdk/assets/system-types/InterpolatedStringHandlerArgumentAttribute.cs
@@ -0,0 +1,27 @@
+// Copyright (c) 2020-2025 SharpCrafters s.r.o. and contributors.
+// SharpCrafters s.r.o. licenses this file to you under either the MIT license or a proprietary license, depending on the repository from which it was obtained.
+// Refer to LICENSE.md in the repository root for complete details.
+
+#if !NET6_0_OR_GREATER
+using JetBrains.Annotations;
+
+// ReSharper disable once CheckNamespace
+namespace System.Runtime.CompilerServices;
+
+[PublicAPI]
+[AttributeUsage( AttributeTargets.Parameter )]
+public sealed class InterpolatedStringHandlerArgumentAttribute : Attribute
+{
+    public InterpolatedStringHandlerArgumentAttribute( string argument )
+    {
+        this.Arguments = [argument];
+    }
+
+    public InterpolatedStringHandlerArgumentAttribute( params string[] arguments )
+    {
+        this.Arguments = arguments;
+    }
+
+    public string[] Arguments { get; }
+}
+#endif
