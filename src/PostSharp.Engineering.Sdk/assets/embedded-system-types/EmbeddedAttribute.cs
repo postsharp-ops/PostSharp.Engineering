eb126544c7495a7a618bf16e3b4e4adeb7361749
Gael Fraiteur <gael@postsharp.net>
2025-09-15 15:28:28 +0200

    Filling types with [Embedded].

---
+++ src/PostSharp.Engineering.Sdk/assets/embedded-system-types/EmbeddedAttribute.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace Microsoft.CodeAnalysis
+{
+    [AttributeUsage( AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Enum | AttributeTargets.Interface | AttributeTargets.Delegate )]
+    internal sealed partial class EmbeddedAttribute : Attribute
+    {
+    }
+}
