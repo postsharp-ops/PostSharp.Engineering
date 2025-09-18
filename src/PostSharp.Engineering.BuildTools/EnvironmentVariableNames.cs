33650a0ea3b36ad8a6537cef360a7b112d9844fb
Gael Fraiteur <gael@postsharp.net>
2025-09-18 08:28:06 +0200

    Added well-known environment variables.

--- src/PostSharp.Engineering.BuildTools/EnvironmentVariableNames.cs
+++ src/PostSharp.Engineering.BuildTools/EnvironmentVariableNames.cs
@@ -4,23 +4,45 @@ namespace PostSharp.Engineering.BuildTools;
 
 public static class EnvironmentVariableNames
 {
-    public const string TeamCityToken = "TEAMCITY_CLOUD_TOKEN";
-    public const string VsMarketplaceAccessToken = "VS_MARKETPLACE_ACCESS_TOKEN";
-    public const string GitHubToken = "GITHUB_TOKEN";
+    // Our infrastructure
     public const string IsPostSharpOwned = "IS_POSTSHARP_OWNED";
     public const string IsTeamCityAgent = "IS_TEAMCITY_AGENT";
     public const string EngUserName = "ENG_USERNAME";
     public const string SignServerSecret = "SIGNSERVER_SECRET";
-    public const string AzEndpoints = "VSS_NUGET_EXTERNAL_FEED_ENDPOINTS";
-    public const string AzureDevOpsUser = "AZURE_DEVOPS_USER";
-    public const string AzureDevOpsToken = "AZURE_DEVOPS_TOKEN";
-    public const string GitHubReviewerToken = "GITHUB_REVIEWER_TOKEN";
-    public const string GitHubAuthorEmail = "GITHUB_AUTHOR_EMAIL";
-    public const string NuGetOrgApiKey = "NUGET_ORG_API_KEY";
+    public const string DocInvalidationKey = "DOC_API_KEY";
+    public const string DownloadsInvalidationKey = "DOWNLOADS_API_KEY";
+
+    // AWS
     public const string AwsAccessKeyId = "AWS_ACCESS_KEY_ID";
     public const string AwsAccessKeySecret = "AWS_SECRET_ACCESS_KEY";
+
+    // TeamCity
+    public const string TeamCityToken = "TEAMCITY_CLOUD_TOKEN";
+
+    // NuGet.org
+    public const string NuGetOrgApiKey = "NUGET_ORG_API_KEY";
+
+    // GitHub
+    public const string GitHubToken = "GITHUB_TOKEN";
+    public const string GitHubReviewerToken = "GITHUB_REVIEWER_TOKEN";
+    public const string GitHubAuthorEmail = "GITHUB_AUTHOR_EMAIL";
+
+    // VS Marketplace
+    public const string VsMarketplaceAccessToken = "VS_MARKETPLACE_ACCESS_TOKEN";
+
+    // Azure DevOps Feeds
+    public const string AzEndpoints = "VSS_NUGET_EXTERNAL_FEED_ENDPOINTS";
+
+    // TypeSense
     public const string TypeSenseApiKey = "TYPESENSE_API_KEY";
+
+    // Azure
     public const string AzIdentityUserName = "AZ_IDENTITY_USERNAME";
+    public const string AzureDevOpsUser = "AZURE_DEVOPS_USER";
+    public const string AzureDevOpsToken = "AZURE_DEVOPS_TOKEN";
+    public const string AzureClientId = "AZURE_CLIENT_ID";
+    public const string AzureClientSecret = "AZURE_CLIENT_SECRET";
+    public const string AzureTenantId = "AZURE_TENANT_ID";
 
     public static readonly string[] All =
     [
@@ -40,6 +62,11 @@ public static class EnvironmentVariableNames
         AwsAccessKeyId,
         AwsAccessKeySecret,
         TypeSenseApiKey,
-        AzIdentityUserName
+        AzIdentityUserName,
+        AzureClientId,
+        AzureClientSecret,
+        AzureTenantId,
+        DocInvalidationKey,
+        DownloadsInvalidationKey
     ];
 }
