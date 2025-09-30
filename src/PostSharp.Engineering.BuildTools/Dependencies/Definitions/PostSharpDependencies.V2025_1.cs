caa254d2049a126c810f1af9da1fd6e7fae17b7d
Gael Fraiteur <gael@postsharp.net>
2025-09-30 12:36:56 +0200

    Fixed prepublish for the PostSharp dependency.

--- src/PostSharp.Engineering.BuildTools/Dependencies/Definitions/PostSharpDependencies.V2025_1.cs
+++ src/PostSharp.Engineering.BuildTools/Dependencies/Definitions/PostSharpDependencies.V2025_1.cs
@@ -15,38 +15,32 @@ public static partial class PostSharpDependencies
     [PublicAPI]
     public static class V2025_1
     {
-        private class PostSharpDependencyDefinition : DependencyDefinition
-        {
-            private static readonly TeamCityProjectId _teamCityProjectId = new(
-                $"PostSharpGitHub_{_projectName}{Family.VersionWithoutDots}",
-                "PostSharpGitHub" );
-
-            private static readonly string _distributionBuildId = $"{_teamCityProjectId}_BuildDistribution";
-
-            public PostSharpDependencyDefinition()
-                : base(
-                    Family,
-                    "PostSharpPackage",
-                    $"refs/heads/release/{Family.Version}",
-                    null,
-                    new GitHubRepository( _projectName, _projectName ),
-                    new CiProjectConfiguration(
-                        _teamCityProjectId,
-                        new ConfigurationSpecific<string>( "not-used", _distributionBuildId, "not-used" ),
-                        null,
-                        null,
-                        EnvironmentVariableNames.TeamCityToken,
-                        TeamCityHelper.TeamCityCloudUrl ),
-                    false )
-            {
-                this.EngineeringDirectory = @"Build\Distribution\eng";
-                this.PackagePatterns = ["PostSharp", "PostSharp.Redist", "PostSharp.Compiler.*", "PostSharp.Patterns.*", "PostSharp.Settings.*"];
-                this.AutoUpdateVersion = false;
-            }
-        }
-
         public static ProductFamily Family { get; } = new( _projectName, "2025.1", DevelopmentDependencies.Family );
 
-        public static DependencyDefinition PostSharp { get; } = new PostSharpDependencyDefinition();
+        private static readonly TeamCityProjectId _teamCityProjectId = new(
+            $"PostSharpGitHub_{_projectName}{Family.VersionWithoutDots}",
+            "PostSharpGitHub" );
+
+        private static readonly string _distributionBuildId = $"{_teamCityProjectId}_BuildDistribution";
+
+        public static DependencyDefinition PostSharp { get; } = new DependencyDefinition(
+            Family,
+            "PostSharpPackage",
+            $"refs/heads/release/{Family.Version}",
+            null,
+            new GitHubRepository( _projectName, _projectName ),
+            new CiProjectConfiguration(
+                _teamCityProjectId,
+                new ConfigurationSpecific<string>( "not-used", _distributionBuildId, "not-used" ),
+                null,
+                null,
+                EnvironmentVariableNames.TeamCityToken,
+                TeamCityHelper.TeamCityCloudUrl ),
+            false )
+        {
+            EngineeringDirectory = @"Build\Distribution\eng",
+            PackagePatterns = ["PostSharp", "PostSharp.Redist", "PostSharp.Compiler.*", "PostSharp.Patterns.*", "PostSharp.Settings.*"],
+            AutoUpdateVersion = false
+        };
     }
 }
--- src/PostSharp.Engineering.BuildTools/Dependencies/Definitions/PostSharpDependencies.V2026_0.cs
+++ src/PostSharp.Engineering.BuildTools/Dependencies/Definitions/PostSharpDependencies.V2026_0.cs
@@ -15,38 +15,32 @@ public static partial class PostSharpDependencies
     [PublicAPI]
     public static class V2026_0
     {
-        private class PostSharpDependencyDefinition : DependencyDefinition
-        {
-            private static readonly TeamCityProjectId _teamCityProjectId = new(
-                $"PostSharpGitHub_{_projectName}{Family.VersionWithoutDots}",
-                "PostSharpGitHub" );
-
-            private static readonly string _distributionBuildId = $"{_teamCityProjectId}_BuildDistribution";
-
-            public PostSharpDependencyDefinition()
-                : base(
-                    Family,
-                    "PostSharpPackage",
-                    $"refs/heads/release/{Family.Version}",
-                    null,
-                    new GitHubRepository( _projectName, _projectName ),
-                    new CiProjectConfiguration(
-                        _teamCityProjectId,
-                        new ConfigurationSpecific<string>( "not-used", _distributionBuildId, "not-used" ),
-                        null,
-                        null,
-                        EnvironmentVariableNames.TeamCityToken,
-                        TeamCityHelper.TeamCityCloudUrl ),
-                    false )
-            {
-                this.EngineeringDirectory = @"Build\Distribution\eng";
-                this.PackagePatterns = ["PostSharp", "PostSharp.Redist", "PostSharp.Compiler.*", "PostSharp.Patterns.*", "PostSharp.Settings.*"];
-                this.AutoUpdateVersion = false;
-            }
-        }
-
         public static ProductFamily Family { get; } = new( _projectName, "2026.0", DevelopmentDependencies.Family );
 
-        public static DependencyDefinition PostSharp { get; } = new PostSharpDependencyDefinition();
+        private static readonly TeamCityProjectId _teamCityProjectId = new(
+            $"PostSharpGitHub_{_projectName}{Family.VersionWithoutDots}",
+            "PostSharpGitHub" );
+
+        private static readonly string _distributionBuildId = $"{_teamCityProjectId}_BuildDistribution";
+
+        public static DependencyDefinition PostSharp { get; } = new DependencyDefinition(
+            Family,
+            "PostSharpPackage",
+            $"refs/heads/release/{Family.Version}",
+            null,
+            new GitHubRepository( _projectName, _projectName ),
+            new CiProjectConfiguration(
+                _teamCityProjectId,
+                new ConfigurationSpecific<string>( "not-used", _distributionBuildId, "not-used" ),
+                null,
+                null,
+                EnvironmentVariableNames.TeamCityToken,
+                TeamCityHelper.TeamCityCloudUrl ),
+            false )
+        {
+            EngineeringDirectory = @"Build\Distribution\eng",
+            PackagePatterns = ["PostSharp", "PostSharp.Redist", "PostSharp.Compiler.*", "PostSharp.Patterns.*", "PostSharp.Settings.*"],
+            AutoUpdateVersion = false
+        };
     }
 }
