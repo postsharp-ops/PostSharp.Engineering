6e3e7408e5c4500b0313e495a38da94342ba0075
Gael Fraiteur <gael@postsharp.net>
2025-10-03 18:48:25 +0200

    Docker: enable long paths for Git.

--- src/PostSharp.Engineering.BuildTools/Docker/GitComponent.cs
+++ src/PostSharp.Engineering.BuildTools/Docker/GitComponent.cs
@@ -20,6 +20,8 @@ public class GitComponent : ContainerComponent
                 $pathsToAdd = @('C:\git\cmd', 'C:\git\bin', 'C:\git\usr\bin'); `
                 $newPath = [Environment]::GetEnvironmentVariable('PATH', 'Machine') + ';' + ($pathsToAdd -join ';'); `
                 [Environment]::SetEnvironmentVariable('PATH', $newPath, 'Machine');
+                
+            RUN "C:\Git\cmd\git.exe" config --system core.longpaths true
             """ );
     }
 }
