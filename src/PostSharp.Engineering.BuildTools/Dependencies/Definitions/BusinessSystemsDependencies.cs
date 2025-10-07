1708d232a2ba470b9318b39a7e8b018602410e45
Gael Fraiteur <gael@postsharp.net>
2025-10-07 17:37:40 +0200

    Fixed dependencies.

--- src/PostSharp.Engineering.BuildTools/Dependencies/Definitions/BusinessSystemsDependencies.cs
+++ src/PostSharp.Engineering.BuildTools/Dependencies/Definitions/BusinessSystemsDependencies.cs
@@ -28,11 +28,17 @@ public static class BusinessSystemsDependencies
 
     public static ProductFamily Family { get; } = new( "Business%20Systems", "1.0", DevelopmentDependencies.Family );
 
-    public static DependencyDefinition BusinessSystems { get; } = new BusinessSystemsDependencyDefinition( "BusinessSystems" );
+    public static DependencyDefinition BusinessSystems { get; } = new BusinessSystemsDependencyDefinition( "BusinessSystems" )
+    {
+        Dependencies = [DevelopmentDependencies.PostSharpEngineering, PostSharpDependencies.V2025_1.PostSharp]
+    };
 
     public static DependencyDefinition HelpBrowser { get; } = new BusinessSystemsDependencyDefinition( "HelpBrowser" );
 
-    public static DependencyDefinition PostSharpWeb { get; } = new BusinessSystemsDependencyDefinition( "PostSharpWeb" );
+    public static DependencyDefinition PostSharpWeb { get; } = new BusinessSystemsDependencyDefinition( "PostSharpWeb" )
+    {
+        Dependencies = [DevelopmentDependencies.PostSharpEngineering, PostSharpDependencies.V2025_1.PostSharp]
+    };
 
     public static DependencyDefinition MetalamaMarketplace { get; } = new BusinessSystemsDependencyDefinition( "MetalamaMarketplace", true );
 }
