b20585295adf6f29ba0447d87821e7939a5c6264
Gael Fraiteur <gael@postsharp.net>
2026-02-01 08:56:26 +0100

    Fixed PreferredVersions.cs

--- src/PostSharp.Engineering.BuildTools/Build/PreferredVersions.cs
+++ src/PostSharp.Engineering.BuildTools/Build/PreferredVersions.cs
@@ -16,17 +16,17 @@ public static class PreferredVersions
 {
     public static class DotNetSdk
     {
-        public const string V_10_0 = "8.0.102";
+        public const string V_10_0 = "10.0.102";
         public const string V_9_0 = "9.0.310";
-        public const string V_8_0 = "10.0.417";
+        public const string V_8_0 = "8.0.417";
         public const string V_6_0 = "6.0.428";
     }
 
     public static class DotNet
     {
-        public const string V_10_0 = "8.0.2";
+        public const string V_10_0 = "10.0.2";
         public const string V_9_0 = "9.0.12";
-        public const string V_8_0 = "10.0.12";
+        public const string V_8_0 = "8.0.23";
         public const string V_6_0 = "6.0.36";
     }
 }   
